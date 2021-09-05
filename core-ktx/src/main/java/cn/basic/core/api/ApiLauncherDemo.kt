42c0a875166267881e1d05cbfa7c0e7a0ff8d317
fqiang <lvfaqiang@gmail.com>
2021-09-05 21:18:59 +0800

    API: 移除with Context 方法包裹。

--- core-ktx/src/main/java/cn/basic/core/api/ApiLauncherDemo.kt
+++ core-ktx/src/main/java/cn/basic/core/api/ApiLauncherDemo.kt
@@ -1,15 +1,13 @@
 package cn.basic.core.api
 
 import cn.basic.core.api.config.CustomException
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.withContext
 
 
 object ApiLauncherDemo {
 
     suspend fun <T> launchCommon(call: suspend () -> T): Results<T> {
         return try {
-            val t = withContext(Dispatchers.Default) { call.invoke() }
+            val t = call.invoke()   // 这里不需要再次用 异步包裹，调用接口时，自动会切换异步。
             Results.success(t)
         } catch (e: Exception) {
             Results.failure(e)
@@ -18,7 +16,7 @@ object ApiLauncherDemo {
 
     suspend fun <T> launch(call: suspend () -> BaseResp<T>): Results<T> {
         return try {
-            val respData = withContext(Dispatchers.Default) { call.invoke() }
+            val respData = call.invoke()     // 这里不需要再次用 异步包裹，调用接口时，自动会切换异步。
             if (respData.success || respData.code == "0") {
                 if (respData.data != null) {
                     Results.success(respData.data)
